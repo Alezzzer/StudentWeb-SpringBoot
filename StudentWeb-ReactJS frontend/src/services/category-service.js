7452eb3699c61fd330da6205fbefa35203803365
AleksandarZeradjanin <134930369+Alezzzer@users.noreply.github.com>
2024-12-22 18:20:15 +0100

    Delete StudentWeb-ReactJS frontend directory

--- StudentWeb-ReactJS frontend/src/services/category-service.js
+++
@@ -1,7 +0,0 @@
-import { myaxios } from "./helper";
-
-export const LoadAllCategoriesFunc = () => {
-  return myaxios.get("/api/categories").then((response) => {
-    return response.data;
-  });
-};
